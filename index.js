2e0e9b3f3454044f34d1217efc321cc4ee90ff47
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 16:21:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function el(){
+    if (el.revenue > revenue){
+      return el
+    }
+  })
+}
